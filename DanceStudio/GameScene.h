842d890cc3b1d62146a4ba30dd6ddccf5fa59e13
Kevin Gaunt <kgaunt@kevinsieolaptop.ideo.com>
2014-09-24 16:19:46 -0700

    Initial Commit

---
+++ DanceStudio/GameScene.h
@@ -0,0 +1,13 @@
+//
+//  GameScene.h
+//  DanceStudio
+//
+
+//  Copyright (c) 2014 IDEO. All rights reserved.
+//
+
+#import <SpriteKit/SpriteKit.h>
+
+@interface GameScene : SKScene
+
+@end
---
+++ DanceStudio/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  DanceStudio
+//
+//  Created by Kevin Gaunt on 9/24/14.
+//  Copyright (c) 2014 IDEO. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
