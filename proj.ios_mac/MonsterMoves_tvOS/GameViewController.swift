8b3d065df55568bcd83f9488d40d4d4f78775094
Poojan Jhaveri <jhaveripoojan@gmail.com>
2015-12-01 15:57:51 -0800

    Adding assets to monster moves tvOS

--- proj.ios_mac/MonsterMoves_tvOS/GameViewController.swift
+++ proj.ios_mac/MonsterMoves_tvOS/GameViewController.swift
@@ -17,15 +17,13 @@ class GameViewController: UIViewController {
         let scene = IntroScene(size:CGSize(width: 1920, height: 1080))
             // Configure the view.
             let skView = self.view as! SKView
-            skView.showsFPS = true
-            skView.showsNodeCount = true
             
             /* Sprite Kit applies additional optimizations to improve rendering performance */
             skView.ignoresSiblingOrder = true
             
             /* Set the scale mode to scale to fit the window */
             scene.scaleMode = .AspectFill
-            
+        
             skView.presentScene(scene)
     }
 
--- proj.ios_mac/MonsterMoves_tvOS/SpaceshipScene.swift
+++ proj.ios_mac/MonsterMoves_tvOS/SpaceshipScene.swift
@@ -10,33 +10,41 @@ import Foundation
 import SpriteKit
 class SpaceshipScene: SKScene {
     
+    var backgroundArray : NSArray = []
+    var characters : NSArray = []
+    
     override func didMoveToView(view: SKView) {
-    /* Setup your scene here */
-    
-    let center = CGPoint(
-        x: CGRectGetMidX(scene!.frame),
-        y: CGRectGetMidY(scene!.frame))
-    
-    let background = SKSpriteNode(imageNamed: "Space")
-    background.position = center
-    background.zPosition = -1
-    scene?.addChild(background)
-    
+        /* Setup your scene here */
         
+        backgroundArray = ["Candy","Desert","Jungle","Space","Ocean","Yay"]
+        characters = ["Le Blob","Sausalito","Meep","Pom"]
         
-    let wait = SKAction.waitForDuration(0.3)
-    let run = SKAction.runBlock {
-        self.spaceshipFlyInAndDropEggs()
+        let getRandom = randomSequenceGenerator(0, max: backgroundArray.count-1)
+        
+        let center = CGPoint(
+            x: CGRectGetMidX(scene!.frame),
+            y: CGRectGetMidY(scene!.frame))
+        
+        let background = SKSpriteNode(imageNamed: backgroundArray[getRandom()] as! String)
+        background.position = center
+        background.zPosition = -1
+        scene?.addChild(background)
+        
+        let wait = SKAction.waitForDuration(0.3)
+        let run = SKAction.runBlock {
+            self.spaceshipFlyInAndDropEggs()
+        }
+        self.runAction(SKAction.sequence([wait,run,wait,]))
     }
-    self.runAction(SKAction.sequence([wait,run]))
+ 
     
-    }
-    
+    // MARK: - Spaceship Methods
     
     func spaceshipFlyInAndDropEggs()
     {
         let spaceship = SKSpriteNode(imageNamed: "Spaceship")
         spaceship.setScale(0.2)
+        spaceship.name = "spaceship"
         spaceship.position = CGPoint(
             x: 150,
             y: scene!.frame.size.height-200)
@@ -44,10 +52,12 @@ class SpaceshipScene: SKScene {
         
         let group =  SKAction.group(
             [
-                SKAction.scaleTo(1.0, duration: 1.0),
+                SKAction.scaleTo(1.0, duration: 0.3),
 //                SKAction.rotateToAngle(-20, duration: 0.6),
                 SKAction.moveTo(CGPoint(x:CGRectGetMidX(scene!.frame), y: scene!.frame.size.height-160), duration: 0.6)
             ])
+        
+        self.runAction(SKAction.playSoundFileNamed("sound/common/FlyInAndDrop.mp3", waitForCompletion: false))
         
         spaceship.runAction(SKAction.sequence(
             [
@@ -71,6 +81,44 @@ class SpaceshipScene: SKScene {
         
     }
     
+    func spaceshipFlyInAndTakeAwayEggs()
+    {
+        let spaceship = self.childNodeWithName("spaceship")
+        if((spaceship) != nil)
+        {
+             self.runAction(SKAction.playSoundFileNamed("sound/common/FlyAway.mp3", waitForCompletion: false))
+            
+            let group =  SKAction.group(
+                [
+                    SKAction.scaleTo(1.0, duration: 0.3),
+                    //                SKAction.rotateToAngle(-20, duration: 0.6),
+                    SKAction.moveTo(CGPoint(x:CGRectGetMidX(scene!.frame), y: scene!.frame.size.height-160), duration: 0.6)
+                ])
+            
+            self.runAction(SKAction.playSoundFileNamed("sound/common/FlyInAndDrop.mp3", waitForCompletion: false))
+            
+            spaceship!.runAction(SKAction.sequence(
+                [
+                    group
+                    ,
+                    
+                    //                SKAction.rotateByAngle(20, duration: 0.0),
+                    SKAction.runBlock({self.takeAwayEggs()}),
+                    SKAction.group(
+                        [
+                            SKAction.moveToY(scene!.frame.size.height-260, duration: 2.0)
+                        ]),
+                    SKAction.group(
+                        [
+                            //                        SKAction.rotateByAngle(53, duration: 0.6),
+                            SKAction.moveTo(CGPoint(x: scene!.frame.size.width+350, y: scene!.frame.size.height+350), duration: 0.6),
+                            SKAction.scaleTo(0.2, duration: 1.0)
+                        ])
+                ]
+                ))
+        }
+    }
+    
     func dropEgg()
     {
         
@@ -86,4 +134,26 @@ class SpaceshipScene: SKScene {
 //        self.addChild(egg)
 
     }
+    
+    func takeAwayEggs()
+    {
+        
+    }
+    
+    // MARK: - Other methods
+
+    func randomSequenceGenerator(min: Int, max: Int) -> () -> Int {
+        var numbers: [Int] = []
+        return {
+            if numbers.count == 0 {
+                numbers = Array(min ... max)
+            }
+            
+            let index = Int(arc4random_uniform(UInt32(numbers.count)))
+            return numbers.removeAtIndex(index)
+        }
+    }
+    
+    
+    
 }
